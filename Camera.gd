ebfaa445ae2ef2c0917afebc94abae18b04e6934
hexaron <stegemann949@gmail.com>
2019-10-08 23:38:54 +0200

    Camera.gd deleted as it wasn't used by any Node

--- Camera.gd
+++
@@ -1,9 +0,0 @@
-extends Camera
-
-onready var player_camera: Camera = $"../World/Player/Rotation_Helper/Camera"
-
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	self.global_transform = player_camera.global_transform
